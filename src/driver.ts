0cc5720c99a942d570d133c44f0f401566b087fd
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2022-11-21 12:12:44 -0600

    fix(driver): add path to .env file to fix running from a different directory

--- src/driver.ts
+++ src/driver.ts
@@ -1,7 +1,7 @@
 import Camera from "./camera";
 import path from "path";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const SETTINGS_FILE_LOC = process.env.SETTINGS_FILE_LOC || path.join(__dirname, "..", "..", "config", "settings.json");
 
